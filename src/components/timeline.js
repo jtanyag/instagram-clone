dfe434572320506bcf03178c4f87a2481800ed26
jtanyag <jtanyag@gmail.com>
2021-07-16 08:51:00 -0700

    refactors timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,18 +1,20 @@
+import { useContext } from 'react';
 import Skeleton from 'react-loading-skeleton';
+import LoggedInUserContext from '../context/logged-in-user';
 import usePhotos from '../hooks/use-photos';
 import Post from './post';
 
 export default function Timeline() {
-  const { photos } = usePhotos();
+  const { user } = useContext(LoggedInUserContext);
+  const { photos } = usePhotos(user);
+  
   return (
     <div className='container col-span-2'>
       {!photos ? (
         <Skeleton count={4} width={640} height={600} className='mb-5' />
-      ) : photos && photos.length > 0 ? (
-        photos.map(content => <Post key={content.docId} content={content} />)
       ) : (
-        <p className='text-center text-2xl'>Follow people to see photos</p>
+        photos.map(content => <Post key={content.docId} content={content} />)
       )}
     </div>
-  )
+  );
 };
